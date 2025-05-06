a5a340a4195d993bcca0a2cce07e884d1b25f095
Mehdi Golkar Amoli <87390273+MehdiGolkarAmoli@users.noreply.github.com>
2025-05-06 12:18:18 +0330

    Update app.py

--- app.py
+++ app.py
@@ -464,7 +464,7 @@ def load_model(model_path):
             decoder_attention_type='scse'
         ).to(device)
         
-        checkpoint = torch.load(model_path, map_location=device)
+        checkpoint = torch.load(model_path, map_location=device, weights_only=False)
         model.load_state_dict(checkpoint['model_state_dict'])
         model.eval()
         return model, device
